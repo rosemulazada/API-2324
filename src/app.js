8af53126afa68b1fe9760cec20db3a3984aa546e
rosemulazada <rosemulazada16@gmail.com>
2024-04-03 14:26:01 +0200

    INIT

---
+++ src/app.js
@@ -0,0 +1,39 @@
+// Core modules
+const path = require("path");
+const fs = require("fs");
+
+// NPM modules
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup EJS engine and views location
+app.set("view engine", "ejs");
+app.set("views", viewsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+/**========================================================================
+ *                           CODE START
+ *========================================================================**/
+
+/**============================================
+ *                   ROUTING
+ *=============================================**/
+
+app.get("", (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
+
+module.exports = app;
