335944a21c71f05c5c275be1a33f030f7e76d12f
rosemulazada <rosemulazada16@gmail.com>
2024-04-04 14:29:55 +0200

    FEAT: Fetch Spotify data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // Core modules
 const path = require("path");
 const fs = require("fs");
+require("dotenv").config();
 
 // NPM modules
 const express = require("express");
@@ -32,8 +33,61 @@ app.get("", (req, res) => {
     res.render("index");
 });
 
+app.get("/genres", async (req, res) => {
+    try {
+        const token = await APIController._getToken();
+        const genres = await APIController._getGenres(token);
+        res.json(genres);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
 
 module.exports = app;
+
+/**============================================
+ *                  API CALL
+ *=============================================**/
+// referenced this video by Avery Wicks on how to use Spotify's API https://www.youtube.com/watch?v=SbelQW2JaDQ
+const APIController = (() => {
+    const clientId = process.env.CLIENT_ID;
+    const clientSecret = process.env.CLIENT_SECRET;
+
+    // private methods
+    const _getToken = async () => {
+        const result = await fetch(`https://accounts.spotify.com/api/token`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: `Basic ${btoa(clientId + ":" + clientSecret)}`,
+            },
+            body: "grant_type=client_credentials",
+        });
+
+        const data = await result.json();
+        console.log(data);
+        return data.access_token;
+    };
+
+    const _getGenres = async (token) => {
+        const result = await fetch(
+            "https://api.spotify.com/v1/browse/categories?locale=sv_US",
+            {
+                method: "GET",
+                headers: { Authorization: `Bearer ${token}` },
+            }
+        );
+
+        const data = await result.json();
+        return data.categories;
+    };
+
+    return {
+        _getToken,
+        _getGenres,
+    };
+})();
