2a21301485e9c7d1d73ef0c435c51f13579bdfba
rosemulazada <rosemulazada16@gmail.com>
2024-04-05 21:41:43 +0200

    FEAT: oAuth logging user data

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 // Core modules
 const path = require("path");
 const fs = require("fs");
+var crypto = require("crypto");
 require("dotenv").config();
 
 // NPM modules
 const express = require("express");
 const querystring = require("querystring");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
 // const ejs = require("ejs");
 
 const app = express();
@@ -20,7 +23,7 @@ app.set("view engine", "ejs");
 app.set("views", viewsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath));
+app.use(express.static(publicDirectoryPath)).use(cookieParser());
 
 /**========================================================================
  *                           CODE START
@@ -36,9 +39,6 @@ app.get("", (req, res) => {
 
 app.listen(port, async () => {
     console.log(`Server running on port ${port}.`);
-    const token = await _getToken();
-    const genres = await _getGenres(token);
-    console.log(genres);
 });
 
 module.exports = app;
@@ -50,21 +50,21 @@ module.exports = app;
 
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
+var stateKey = "spotify_auth_state";
+
 const redirect_uri =
     process.env.NODE_ENV == "dev"
         ? "http://localhost:8000/callback"
-        : "https://api-2324.vercel.app//callback";
+        : "https://api-2324.vercel.app/callback";
 
 const generateRandomString = (length) => {
-    const possible =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const values = crypto.getRandomValues(new Uint8Array(length));
-    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+    return crypto.randomBytes(60).toString("hex").slice(0, length);
 };
 
 app.get("/login", function (req, res) {
     var state = generateRandomString(16);
-    var scope = "user-read-email user-top-read";
+    var scope = "user-read-private user-top-read";
+    res.cookie(stateKey, state);
 
     res.redirect(
         "https://accounts.spotify.com/authorize?" +
@@ -78,36 +78,87 @@ app.get("/login", function (req, res) {
     );
 });
 
-// private methods
-const _getToken = async () => {
-    const result = await fetch(`https://accounts.spotify.com/api/token`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: `Basic ${btoa(client_id + ":" + client_secret)}`,
-        },
-        body: "grant_type=client_credentials",
-    });
+app.get("/callback", function (req, res) {
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
 
-    const data = await result.json();
-    console.log(data);
-    return data.access_token;
-};
+    if (state === null || state !== storedState) {
+        res.redirect(
+            "/#" +
+                querystring.stringify({
+                    error: "state_mismatch",
+                })
+        );
+    } else {
+        res.clearCookie(stateKey);
+        var authOptions = {
+            url: "https://accounts.spotify.com/api/token",
+            data: querystring.stringify({
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: "authorization_code",
+            }),
+            headers: {
+                "content-type": "application/x-www-form-urlencoded",
+                Authorization:
+                    "Basic " +
+                    new Buffer.from(client_id + ":" + client_secret).toString(
+                        "base64"
+                    ),
+            },
+        };
 
-const _getGenres = async (token) => {
-    const result = await fetch(
-        "https://api.spotify.com/v1/browse/categories?locale=US",
-        {
-            method: "GET",
-            headers: { Authorization: `Bearer ${token}` },
-        }
-    );
+        axios
+            .post(authOptions.url, authOptions.data, {
+                headers: authOptions.headers,
+            })
+            .then(function (response) {
+                if (response.status === 200) {
+                    var access_token = response.data.access_token,
+                        refresh_token = response.data.refresh_token;
 
-    const data = await result.json();
-    return data.categories.items;
-};
+                    var options = {
+                        url: "https://api.spotify.com/v1/me",
+                        headers: { Authorization: "Bearer " + access_token },
+                    };
 
-return {
-    _getToken,
-    _getGenres,
-};
+                    // use the access token to access the Spotify Web API
+                    axios
+                        .get(options.url, { headers: options.headers }) // Use axios.get
+                        .then(function (response) {
+                            console.log(response.data);
+                        })
+                        .catch(function (error) {
+                            console.error(error);
+                        });
+
+                    // we can also pass the token to the browser to make requests from there
+                    res.redirect(
+                        "/#" +
+                            querystring.stringify({
+                                access_token: access_token,
+                                refresh_token: refresh_token,
+                            })
+                    );
+                } else {
+                    res.redirect(
+                        "/#" +
+                            querystring.stringify({
+                                error: "invalid_token",
+                            })
+                    );
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                res.redirect(
+                    "/#" +
+                        querystring.stringify({
+                            error: "invalid_token",
+                        })
+                );
+                
+            });
+    }
+});
